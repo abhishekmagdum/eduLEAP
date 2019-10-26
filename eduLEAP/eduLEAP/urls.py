ade5b0a938304297eef2327bdd377e7d92c67cd2
Adrian Sochaniwsky <adr.soch@outlook.com>
2019-10-25 23:55:32 -0400

    Updated files - bug exists

--- eduLEAP/eduLEAP/urls.py
+++ eduLEAP/eduLEAP/urls.py
@@ -18,4 +18,21 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
 ]
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
